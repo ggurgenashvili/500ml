6f5ef3cf9d7ac9c464d35460c809ec0e3d408167
g.gurgenashvili <g.gurgenashvili@paybox.ge>
2024-05-30 13:49:11 +0400

    input

--- cypress/e2e/first.spec.js
+++ cypress/e2e/first.spec.js
@@ -9,9 +9,16 @@ describe('500ml', () => {
 
   })
   it.only('second test: contact us', () => {
-    cy.visit('https://500ml.vercel.app/');
-    cy.get('.contactUs').click();
+    cy.visit('https://500ml.vercel.app/')
+    cy.get('.contactUs').click()
     cy.wait(4000)
-  cy.get('span').contains('First Name').type('name', { force: true });
+    // cy.get('input').contains('First Name').type('name', { force: true });
+    cy.contains('.InputBox', 'First Name').find('input').type('guli')
+    cy.contains('.InputBox', 'Last Name').find('input').type('gurgenashvili')
+    cy.contains('.BigInputBox', 'Email').find('input').type('test@test.com')
+    cy.contains('.BigInputBox', 'Phone').find('input').type('12345')
+    cy.get('.DescribeIssueBox').type('issue box')
+    cy.get('.Button_send').click()
+
   })
 })
